436f7449bd766c93aba48b62322d6f95e3517406
adam-weiler <adam.weiler@yahoo.ca>
2019-07-17 17:42:15 -0400

    Adding init instance method, and create instance method.

--- Book.py
+++ Book.py
@@ -25,3 +25,59 @@ class Book():
     #These are class variables.
     on_shelf = []
     on_loan = []
+
+
+    #These are instance methods.
+    def __init__(self, title, author, ISBN):
+        self.title = title
+        self.author = author
+        self.ISBN = ISBN
+
+    def borrow(self):
+        pass
+
+    def return_to_library(self):
+        pass
+
+    def lent_out(self):
+        pass
+
+
+    #These are class methods.
+    @classmethod
+    def create(cls, title, author, ISBN):
+        new_book = Book(title, author, ISBN)
+        cls.on_shelf.append(new_book)
+        return new_book
+
+    def current_due_date(cls):
+        pass
+
+    def overdue_books(cls):
+        pass
+
+    def browse(cls):
+        pass
+
+
+
+
+sister_outsider = Book.create("Sister Outsider", "Audre Lorde", "9781515905431")
+aint_i = Book.create("Ain't I a Woman?", "Bell Hooks", "9780896081307")
+if_they_come = Book.create("If They Come in the Morning", "Angela Y. Davis", "0893880221")
+# print(Book.browse().title) # "Sister Outsider" (this value may be different for you)
+# print(Book.browse().title) # "Ain't I a Woman?" (this value may be different for you)
+# print(len(Book.on_shelf)) # 3
+# print(len(Book.on_loan)) # 0
+# print(sister_outsider.lent_out()) # False
+# print(sister_outsider.borrow()) # True
+# print(len(Book.on_shelf)) # 2
+# print(len(Book.on_loan)) # 1
+# print(sister_outsider.lent_out()) # True
+# print(sister_outsider.borrow()) # False
+# print(sister_outsider.due_date) # 2017-02-25 20:52:20 -0500 (this value will be different for you)
+# print(len(Book.overdue())) # 0
+# print(sister_outsider.return_to_library()) # True
+# print(sister_outsider.lent_out()) # False
+# print(len(Book.on_shelf)) # 2
+# print(len(Book.on_loan)) # 0
