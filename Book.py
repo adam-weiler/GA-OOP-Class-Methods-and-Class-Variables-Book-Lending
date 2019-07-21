f96462aa9f5e61217a412eb107a283338db44b47
adam-weiler <adam.weiler@yahoo.ca>
2019-07-21 17:00:06 -0400

    Project complete. Added search method for title, author, ISBN, or genre.

--- Book.py
+++ Book.py
@@ -1,10 +1,9 @@
-from datetime import date #Used to calculate due dates.
-from datetime import datetime #Used to calculate due dates.
-from datetime import timedelta #Used to calculate due dates.
+from datetime import date, datetime #Used to calculate due dates.
 import random #Used to choose a random book from the shelf.
 
 class Book():
     #These are class variables.
+    entire_library = []
     on_shelf = []
     on_loan = []
 
@@ -29,7 +28,6 @@ class Book():
         #The loop is over and the book was not found.
         # print(f'\'{self.title}\' is already lent out.') #This line is not necessary.
         return True #The book has been taken out.
-
 
     def borrow(self):
         if self.lent_out(): #The book is already lent out.
@@ -56,7 +54,6 @@ class Book():
             print('The book wasn\'t on loan in the first place.')
             return False
 
-
     def renew(self): #Renews the book by setting the due_date for 2 weeks from now.
         if (self.lent_out()): #If lent_out is true.
             if (self.on_hold): #If book is on hold.
@@ -71,13 +68,11 @@ class Book():
             print('The book wasn\'t on loan in the first place.')
             return False
 
-
-
-            
     #These are class methods.
     @classmethod
     def create(cls, title, author, ISBN, genre):
         new_book = Book(title, author, ISBN, genre)
+        cls.entire_library.append(new_book)
         cls.on_shelf.append(new_book)
         print(new_book)
         return new_book
@@ -91,9 +86,6 @@ class Book():
 
     @classmethod
     def overdue_books(cls):
-        # print('\n Overdue book')
-        # print(cls.on_loan[0].due_date)
-
         overdue_list = []
         
         for book in cls.on_loan: #Iterates through each book in the on_loan list.
@@ -102,50 +94,76 @@ class Book():
                 overdue_list.append(book)
         return overdue_list
 
-
     @classmethod
     def browse(cls): #Returns a random book from the on_shelf list.
         return random.choice(cls.on_shelf)
 
+    @classmethod
+    def search_books(cls, user_input, trait):
+        print(f'Looking for {trait} - {user_input}:')
 
+        if trait == 'Author':
+            for book in cls.entire_library:
+                if book.author.find(user_input) >= 0:
+                    print(book)
+        elif trait == 'ISBN':
+            for book in cls.entire_library:
+                if book.ISBN.find(user_input) >= 0:
+                    print(book)
+        elif trait == 'Genre':
+            for book in cls.entire_library:
+                if book.genre == user_input:
+                    print(book)
+        else:
+            for book in cls.entire_library:
+                if book.title.find(user_input) >= 0:
+                    print(book)
+
+
+print('Adding new books to the library:')
 sister_outsider = Book.create('Sister Outsider', 'Audre Lorde', '9781515905431', 'Nonfiction')
-# aint_i = Book.create('Ain\'t I a Woman?', 'Bell Hooks', '9780896081307', 'Nonfiction')
-# if_they_come = Book.create('If They Come in the Morning', 'Angela Y. Davis', '0893880221', 'Nonfiction')
+aint_i = Book.create('Ain\'t I a Woman?', 'Bell Hooks', '9780896081307', 'Nonfiction')
+if_they_come = Book.create('If They Come in the Morning', 'Angela Y. Davis', '0893880221', 'Nonfiction')
 game_of_thrones = Book.create('Game of Thrones', 'George R.R. Martin', '0553588486', 'Fantasy')
 book_1984 = Book.create('1984', 'George Orwell', '9780451524935', 'Fiction')
 hitchikers_guide_to_the_galaxy = Book.create('The Hitchhiker\'s Guide to the Galaxy', 'Douglas Adams', '0345391802', 'Science-Fiction')
 print()
 
-# print(Book.browse().title) # A random book title.
-# print(Book.browse().title)
-# print(len(Book.on_shelf)) # 3
-# print(len(Book.on_loan)) # 0
-# print()
-
-# print(sister_outsider.lent_out()) # False ; Book is not lent out.
-sister_outsider.borrow() # True ; Book is now borrowed.
-# print(len(Book.on_shelf)) # 2
-# print(len(Book.on_loan)) # 1
-# # print(sister_outsider.lent_out()) # True
-# print()
-
-sister_outsider.borrow() # False
-print(sister_outsider.on_hold) # True
-sister_outsider.renew() #False
-print()
-
-
-# print(sister_outsider.due_date) # 2017-02-25 20:52:20 -0500 (this value will be different for you)
-print(len(Book.overdue_books())) # 0
-sister_outsider.return_to_library() # True
-# print(sister_outsider.lent_out()) # False
-print(len(Book.on_shelf)) # 3
+print(Book.browse().title) # A random book title.
+print(Book.browse().title)
+print(len(Book.on_shelf)) # 6
 print(len(Book.on_loan)) # 0
 print()
 
-# print(Book.browse().genre) # A random book genre.
-# print(Book.browse().genre)
-# sister_outsider.renew() #Renews the due_date by 2 weeks.
+print(sister_outsider.lent_out()) # False ; Book is not lent out.
+sister_outsider.borrow() # True ; Book is now borrowed.
+print(len(Book.on_shelf)) # 5
+print(len(Book.on_loan)) # 1
+print(sister_outsider.lent_out()) # True ; Book is lent out.
+sister_outsider.renew() #Renews the due_date by 2 weeks.
+print()
 
+sister_outsider.borrow() # False ; You cannot borrow a book that is out.
+print(sister_outsider.on_hold) # True ; Book is on hold.
+sister_outsider.renew() #False ; You cannot renew a book that is on hold.
+print()
 
-print(sister_outsider)
+print(sister_outsider.due_date) # 2019-08-04 23:59:59
+print(len(Book.overdue_books())) # 0
+sister_outsider.return_to_library() # True - Book is returned.
+print(sister_outsider.lent_out()) # False ; Book is not lent out.
+print()
+
+print(len(Book.on_shelf)) # 6
+print(len(Book.on_loan)) # 0
+print(Book.browse().genre) # A random book genre.
+print(Book.browse().genre)
+print()
+
+Book.search_books('the', 'Title') #Returns 2 books.
+print()
+Book.search_books('George', 'Author') #Returns 2 books.
+print()
+Book.search_books('Nonfiction', 'Genre') #Returns 3 books.
+print()
+Book.search_books('978', 'ISBN') #Returns 3 books.
